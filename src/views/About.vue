19f1218d4aa91a04893089ff8fadb7959fc66b4a
lzw <li495469217@163.com>
2020-02-18 20:53:32 +0800

    学习VUE

---
+++ src/views/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <button type="button" @click="add()">click</button>
+  </div>
+</template>
+
+<script>
+import store from '@/store'
+export default {
+  name: "About",
+  methods: {
+    add() {
+      store.commit('increase');
+    }
+  }
+};
+</script>
+<style scoped>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    {{msg}}
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+import store from '@/store'
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+  data(){
+    return{
+      msg:store.state.count
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
